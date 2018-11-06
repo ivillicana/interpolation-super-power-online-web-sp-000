5df505d5c4babf228b0cd52f6a660e11dd75fd99
Jacob Barry <the.jacobbarry@gmail.com>
2018-11-06 17:29:30 +0000

    automatically backed up by learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,5 +1,5 @@
 require 'pry'
-def display_rainbow(colors)
+def display_rainbow(colors binding.pry)
   colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-binding.pry puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+ puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
